107a6769519c4c9ed5a62e8392f688a3ac241524
GWStuart <89712016+GWStuart@users.noreply.github.com>
2024-06-07 19:23:16 +1000

    added functionalty to the power util

--- Scripts/power_util.sh
+++ Scripts/power_util.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 query="$( echo -e "   poweroff\n󰍁   logout\n󰤄   sleep" | wofi --style="$HOME/.config/wofi/power_style.css" --conf="$HOME/.config/wofi/power_config" --show dmenu)"
+if [ "$query" = "   poweroff" ]; then
+  poweroff
+elif [ "$query" = "󰍁   logout" ]; then 
+  # could do a swaylock or something 
+  echo "not implemented yet"
+elif [ "$query" = "󰤄   sleep" ]; then 
+  systemctl suspend
+fi
