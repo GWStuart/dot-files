97e76642fb225cddb342a07f60aa7ae0b2083046
GWStuart <89712016+GWStuart@users.noreply.github.com>
2024-06-08 17:44:04 +1000

    modified search locations

--- Scripts/find.sh
+++ Scripts/find.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 query="$( python ~/Scripts/find.py | wofi --style="$HOME/.config/wofi/find_style.css" --conf="$HOME/.config/wofi/find_config" --show dmenu)" 
-# python ~/Scripts/find.py | wofi --show dmenu
-alacritty --working-directory=${query}
+
+alacritty --working-directory=$query
+# xdg-open $query
